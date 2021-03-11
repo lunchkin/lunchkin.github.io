91c43d26f1cdddd17ee8ff99b43252e07e1ca6dc
Robert <rkinney333@gmail.com>
2021-03-10 20:00:00 -0500

    Testing deployment. And adding bulma nav- wip

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/my-resume-vue/'
-        : '/'
+    // publicPath: process.env.NODE_ENV === 'production'
+    //     ? '/my-resume-vue/'
+    //     : '/'
+
+    publicPath: '/'
 }
